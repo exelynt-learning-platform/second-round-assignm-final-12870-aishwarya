4b9d5a9f95bbe98e713edbced92aea2247ccbb08
AASHELKE16 <aishwaryashelke9696@gmail.com>
2026-04-01 19:27:18 +0530

    Add files via upload

---
+++ GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.example.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public String handleRuntimeException(RuntimeException ex) {
+        return ex.getMessage();
+    }
+}
